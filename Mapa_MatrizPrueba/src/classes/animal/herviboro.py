8b0752ab4f920f3a47ba3321d4a3baf6a7e714b2
don-python <134964183+don-python@users.noreply.github.com>
2023-11-25 21:53:23 -0300

    Intento de matriz fallido

--- Mapa_MatrizPrueba/src/classes/animal/herviboro.py
+++
@@ -1,5 +0,0 @@
-from classes.animal.animal import Animal
-from classes.planta.planta import Planta
-class Herviboro(Animal):
-    def __init__(self,sprite, hp, nrg, attackRange, visionRange):
-        super().__init__(sprite, hp, nrg, 1, attackRange, visionRange)
--- Mapa_MatrizPrueba/src/classes/planta/planta.py
+++
@@ -1,5 +0,0 @@
-from classes.organismo import Organismo
-
-class Planta (Organismo):
-    def __init__(self,sprite):
-        super().__init__(sprite, 50, 10, 0)
