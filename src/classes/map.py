f49b9a31adab3de159bbc57f3d61ed45a2d84cd4
don-python <134964183+don-python@users.noreply.github.com>
2023-11-26 03:12:53 -0300

    Cambio sprite de pasto

--- src/classes/map.py
+++ src/classes/map.py
@@ -3,25 +3,29 @@ from classes.ecosistema import Ecosistema
 from classes.menu import Menu
 from constants import *
 
+import pygame
+from classes.ecosistema import Ecosistema
+from classes.menu import Menu
+from constants import *
+
 class Map:
     def __init__(self,screen):
         # Principal
         self.screen = screen
         self.boardSurface = pygame.Surface((cellNum * cellSize, cellNum * cellSize))
-        self.menuSurface = pygame.Surface((menuWidth, cellNum * cellSize))
         self.ecosistema = Ecosistema()
         self.menu = Menu()
-
+        self.matrix = map
     def initDisplay(self):
         self.drawBoard(self.boardSurface)
-        
+
     def drawBoard(self,surface):
-        for row in range (cellNum):
-            for col in range (cellNum):
-                if (row + col) % 2 == 0:
-                    sprite = tile1Sprite
-                else:
-                    sprite = tile1Sprite
+        for row in range(len(self.matrix)):
+            for col in range(len(self.matrix[row])):
+                cell = self.matrix[row][col]
+                if cell == 1:
+                    sprite = p1Sprite
+                #elif cell == 2:
                 surface.blit(sprite,(col * cellSize, row * cellSize))
 
     def drawDisplay(self):
